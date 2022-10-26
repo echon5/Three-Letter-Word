e15ae7006c8d00f6288ba5b2cfde41f655e65ace
Erin Chon <echon@bsd220.org>
2022-10-26 09:28:08 -0500

    Initial Commit

---
+++ Three Letter Word/Three_Letter_WordApp.swift
@@ -0,0 +1,17 @@
+//
+//  Three_Letter_WordApp.swift
+//  Three Letter Word
+//
+//  Created by Erin Chon on 10/26/22.
+//
+
+import SwiftUI
+
+@main
+struct Three_Letter_WordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
